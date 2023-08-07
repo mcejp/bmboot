1b14eed2f4fa750792ac851eb7ede09ed33bbd2c
Martin Cejp <martin.cejp@cern.ch>
2023-08-07 11:23:30 +0200

    bmboot: Add timer_demo

---
+++ src/payloads/timer_demo.cpp
@@ -0,0 +1,24 @@
+#include <bmboot/payload_runtime.hpp>
+
+static void myHandler();
+
+int main(int argc, char** argv)
+{
+    bmboot::notifyPayloadStarted();
+
+    printf("hello from payload\n");
+
+    bmboot::startPeriodicInterrupt(1'000'000, myHandler);
+
+    for (;;) {}
+}
+
+static void myHandler()
+{
+    static int cnt = 0;
+    printf("%dth event\n", ++cnt);
+
+    if (cnt == 5) {
+        bmboot::stopPeriodicInterrupt();
+    }
+}
