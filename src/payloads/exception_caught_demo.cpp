98ffb4e4d45387dac07c63786753c542bc210ed8
Dominik Arominski <dominik.arominski@cern.ch>
2023-10-03 10:42:26 +0200

    [FGC4] Bmboot: added test payload for proper exception handling

---
+++ src/payloads/exception_caught_demo.cpp
@@ -0,0 +1,21 @@
+#include <stdexcept>
+
+#include <bmboot/payload_runtime.hpp>
+
+int main(int argc, char** argv)
+{
+    bmboot::notifyPayloadStarted();
+
+    try
+    {
+      throw std::runtime_error("Runtime exception thrown");
+    }
+    catch (std::exception& e)
+    {
+      printf("Caught exception: %s.\n", e.what());
+    }
+    printf("Application still responsive after exception handling.");
+    puts(""); // flushes the stdout
+
+    for (;;) {}
+}
